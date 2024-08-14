12823f2929a0f67598648aae54285c205bef1c84
MuhammadAminShomurodov <kriminaltj@gmail.com>
2024-08-15 01:57:07 +0500

    new

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-import { Navigate, RouteProps } from "react-router-dom";
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
 
-interface PrivateRouteProps extends RouteProps {
+interface PrivateRouteProps {
   element: React.ReactElement;
 }
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ element }) => {
-  const isAuthenticated = localStorage.getItem("auth") === "true";
+  const location = useLocation();
+  const isAuthenticated = localStorage.getItem('auth') === 'true';
 
-  return isAuthenticated ? element : <Navigate to="/login" />;
+  return isAuthenticated ? (
+    element
+  ) : (
+    <Navigate to="/login" state={{ from: location }} />
+  );
 };
 
 export default PrivateRoute;
